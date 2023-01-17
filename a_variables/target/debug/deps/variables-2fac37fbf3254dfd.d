afd9bbc09fe6c3610a61c10b5d4853355c13ad51
Bibek <66242058+ibibekadhikari@users.noreply.github.com>
2023-01-17 20:49:08 +0545

    Deleting a directory from Target.
    It was added during the compilation time so cleaning up the directory.

--- a_variables/target/debug/deps/variables-2fac37fbf3254dfd.d
+++
@@ -1,5 +0,0 @@
-/Users/bibek/Desktop/RustPractice/UdemyExercise/a_variables/target/debug/deps/variables-2fac37fbf3254dfd: src/main.rs
-
-/Users/bibek/Desktop/RustPractice/UdemyExercise/a_variables/target/debug/deps/variables-2fac37fbf3254dfd.d: src/main.rs
-
-src/main.rs:
--- a_variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-/Users/bibek/Desktop/RustPractice/UdemyExercise/rust_challenges/a_variables/target/debug/variables: /Users/bibek/Desktop/RustPractice/UdemyExercise/rust_challenges/a_variables/src/main.rs
